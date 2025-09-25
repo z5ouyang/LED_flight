a7107e9ca3843316e8d202031d809db838a88ec1
O'Young <oyoung122@gmail.com>
2025-09-25 01:23:37 -0700

    add long LED control

--- led_flight.py
+++ led_flight.py
@@ -1,32 +1,20 @@
-import os, subprocess,yaml,requests,time
+import os, subprocess,requests,time
 import utility as ut
+import modbus_led as ml
+import plane_icon as pi
 from datetime import datetime
 from zoneinfo import ZoneInfo
 
-VERBOSE_LEVEL=1 #0: no print out; 1: important; 2: everything
-WAIT_TIME=15
-TZ = ZoneInfo("America/Los_Angeles")
-
-@staticmethod
-def calculate_modbus_crc(data):
-    crc = 0xFFFF
-    for byte in data:
-        crc ^= byte
-        for _ in range(8):
-            if crc & 0x0001:
-                crc = (crc >> 1) ^ 0xA001
-            else:
-                crc >>= 1
-    return crc.to_bytes(2, 'little')
-## send data to modbus with return data #/dev/ttyUSB0
-def send_modbus(port,tx_data,return_size,baudrate=9600,CRC=False):
-    if CRC:
-        tx_data = tx_data + calculate_modbus_crc(tx_data)
-    ser = serial.Serial(port=port,baudrate=baudrate,parity=serial.PARITY_NONE,stopbits=serial.STOPBITS_ONE,bytesize=serial.EIGHTBITS,timeout=1)
-    a = ser.write(tx_data)
-    rx_data = ser.read(size=return_size)
-    ser.close()
-    return rx_data
+VERBOSE_LEVEL=True #0: no print out; 1: important; 2: everything
+TZ = ZoneInfo('America/Los_Angeles')
+LED_CURR_BRIGHTNESS=500
+LED_DAY_BRIGHTNESS=500
+LED_NIGHT_BRIGHTNESS=10
+PLANE_SPEED=0.001
+SHORT_CANVAS=1
+LONG_CANVAS=2
+ALT_CANVAS=3
+FLIP_EAST_WEST=False flip_east_west
 
 def get_serial():
     strSerial = "/proc/device-tree/serial-number"
@@ -49,55 +37,164 @@ def get_serial():
     #    return f"Error: {e}"
     return "0000000000000000"
 
-def 
+def get_wifi_ssid():
+    try:
+        ssid = subprocess.check_output(['iwgetid', '-r']).decode().strip()
+        return ssid if ssid else "Not connected to Wi-Fi"
+    except subprocess.CalledProcessError:
+        return "Wi-Fi SSID not found"
 
+def ping_google(tryN=3):
+    while tryN>0:
+        tryN -=1
+        try:
+            output = subprocess.check_output(['ping', '-c', '1', '8.8.8.8'], stderr=subprocess.STDOUT).decode()
+            return True
+        except subprocess.CalledProcessError as e:
+            if VERBOSE_LEVEL:
+                print(f"Ping failed: {e.output.decode()}")
+    return False
 
 def check_wifi():
-    ping_ip = ipaddress.IPv4Address("8.8.8.8")  # Google's DNS
-    tryN = 3
-    fwifi = False
-    labels = ["WIFI",os.getenv("CIRCUITPY_WIFI_SSID"),'FAILED']
-    while tryN>0:
-        ping = wifi.radio.ping(ip=ping_ip)
-        if ping:
-            fwifi=True
-            labels = ["Connected","to",os.getenv("CIRCUITPY_WIFI_SSID")]
-            break
-        tryN -=1
-    matrixportal.display.root_group = get_text(labels)
+    fwifi = ping_google()
+    if fwifi:
+        ml.show_text(0,0,192,32,'FFF',"Connected to\n%s"%get_wifi_ssid(),True)
+    else:
+        ml.show_text(0,0,192,32,'FFF',"WIFI\nFailed",True)
     time.sleep(5)
     return fwifi
 
+def set_time_zone(tz):
+    global TZ
+    try:
+        TZ = ZoneInfo(tz)
+    except subprocess.CalledProcessError as e:
+        if VERBOSE_LEVEL:
+            print(f"Ping failed: {e.output.decode()}")
+    ml.show_text(0,0,192,32,'FFF',"Local Time Zone\n%s"%datetime.now(TZ).strftime('%Z'),True)
+    time.sleep(5)
 
+def check_brightness(night_time):
+    global LED_CURR_BRIGHTNESS
+    dt = datetime.now(TZ)
+    bNight = (night_time[0]<night_time[1] and night_time[0] <= dt.hour < night_time[1]) or (night_time[0]>night_time[1] and (night_time[0] <= dt.hour or dt.hour< night_time[1]))
+    if bNight and LED_CURR_BRIGHTNESS!=LED_NIGHT_BRIGHTNESS:
+        LED_CURR_BRIGHTNESS=LED_NIGHT_BRIGHTNESS
+        ml.set_brightness(LED_NIGHT_BRIGHTNESS)
+    elif not bNight and LED_CURR_BRIGHTNESS!=LED_DAY_BRIGHTNESS
+        LED_CURR_BRIGHTNESS=LED_DAY_BRIGHTNESS
+        ml.set_brightness(LED_DAY_BRIGHTNESS)
 
+def display_date_time():
+    dt = datetime.now(TZ)
+    ml.show_text(0,0,192,32,"FF0","%s %d\t%s\n\t\t%s"%(dt.strftime('%b'),dt.day,dt.strftime('%a'),dt.strftime('%H:%M')),True)
+
+def plane_animation():
+    img = pi.get_plane_horizontal()
+    w = h = len(img)
+    H = 32
+    W = 192
+    h1 = int((H-h)/2)
+    h2 = H-h-h1
+    for i in range(h1):
+        img.insert(0,"0"*w)
+    for i in range(h1):
+        img.append("0"*w)
+    ml.show_image(W-w,0,img)
+    for i in range(W):
+        ml.move_frame_left(max(0,W-w-i),0,w,H)
+
+def display_alt_sp(fInfo):
+    x=32
+    heading = (360 - int(fInfo['heading']))%360 if FLIP_EAST_WEST else int(fInfo['heading'])
+    img = getattr(pi,'get_plane_'+str(closest_heading(heading)))()
+    w = len(img)
+    ml.show_image(x,2,img)
+    ml.show_text(x+w+2,0,190-x-w,16,'FF0',"%s ft. %s kts"%(str(fInfo['altitude']),str(fInfo['speed'])))
 
 def show_flight(flight_info):
     if VERBOSE_LEVEL>0:
         print(datetime.now(TZ),flight_info)
+    plane_animation()
+    labels_s = [flight_info['flight_number'],flight_info['airports_short'],flight_info['aircraft_code']]
+    labels_l = [flight_info['airline_name'],flight_info['airports_long'],flight_info['aircraft_model']]
+    ml.create_txt_programe(SHORT_CANVAS,'F0F',4,5,200,4,50,'\n'.join(labels_s),True)
+    ml.create_txt_programe(LONG_CANVAS,'F0F',2,5,0,2,20,' '.join(labels_l))
+    display_alt_sp(flight_info)
+    time.sleep(5)
 
-def clear_flight():
+def clear_flight(flight_index):
     if VERBOSE_LEVEL>0:
         print(datetime.now(TZ),"Clear")
+    flight = ut.get_flight_short(requests,flight_index,DEBUG_VERBOSE=DEBUG_VERBOSE)
+    if flight is not None and flight['altitude']<ut.LANDING_ALTITUDE:
+        ml.show_text(0,16,192,16,"0FF","Landed\t"+str(flight["speed"])+' kts')
+    else:
+        ml.show_text(0,16,192,16,"0FF",'Out of Monitor Boundary')
+    time.sleep(5)
+
+def update_flight():
+
+def init(config):
+    global FLIP_EAST_WEST
+    try:
+        ml.get_GID()
+        ml.set_text_color('FF0')
+        ml.create_canvas(SHORT_CANVAS,0,0,64,16)
+        ml.create_canvas(LONG_CANVAS,0,16,192,16)
+        FLIP_EAST_WEST = False if config.get('flip_east_west') is None else config.get('flip_east_west')
+    except subprocess.CalledProcessError as e:
+        if VERBOSE_LEVEL:
+            print(f"LED controller failed: {e.output.decode()}")
+        return False
+    return True
 
 def main():
-    print("Unique serial:",get_serial())
-    config = ut.get_config()
+    config = ut.get_config(config)
+    if not init():
+        return    
+    ml.VERBOSE_LEVEL=VERBOSE_LEVEL
+    if not check_wifi():
+        return
+    set_time_zone(config.get('time_zone'))
     findex_old=None
+    finfo=None
+    flight_followed=ut.MAX_FOLLOW_PLAN
+    gc.collect()
     while True:
-        wait_time = WAIT_TIME
-        findex,req_success = ut.get_flights(requests,config['geo_loc'],config.get('altitude'),config.get('heading'),config.get('center_loc'),DEBUG_VERBOSE=VERBOSE_LEVEL>0)
+        wait_time = ut.WAIT_TIME
+        check_brightness(config.get("display_time_night"))
+        findex,fshort,req_success = ut.get_flights(requests,config['geo_loc'],config.get('altitude'),config.get('heading'),
+            config.get('center_loc'),config.get('dest'),config.get('speed'),DEBUG_VERBOSE=DEBUG_VERBOSE)
+        ## follow the previous plane when it moved outside the boundary but no new plane entered
+        if findex is None and findex_old is not None and flight_followed>0:
+            if DEBUG_VERBOSE:
+                print("=== Follow the previous plane which is out of boundary ===")
+                print(findex,":",findex_old,":",flight_followed)
+            fshort = ut.get_flight_short(requests,findex_old,DEBUG_VERBOSE=DEBUG_VERBOSE)
+            findex = findex_old
+            if fshort['altitude']<ut.LANDING_ALTITUDE:
+                flight_followed=0
+            flight_followed -=1
+        ##
         if findex!=findex_old:
-            findex_old = findex
             if findex is None:
-                clear_flight()
+                clear_flight(findex_old)
+                display_date_time()                
             else:
-                finfo = ut.get_flight_detail(requests,findex,DEBUG_VERBOSE=VERBOSE_LEVEL>0)
+                finfo = ut.get_flight_detail(requests,findex,DEBUG_VERBOSE=DEBUG_VERBOSE)
                 if finfo is not None:                    
                     show_flight(finfo)
-                    wait_time=ut.get_est_arrival(finfo['eta'])
-        if VERBOSE_LEVEL>1:
-            print("\tWaiting",wait_time,"seconds")
+                    wait_time=ut.UPDATE_TIME
+            findex_old = findex
+            flight_followed=ut.MAX_FOLLOW_PLAN
+        elif findex is not None:
+            if DEBUG_VERBOSE:
+                print(findex,":",findex_old)
+            update_flight(fshort)
+            wait_time=ut.UPDATE_TIME
+        else:
+            display_date_time()
         time.sleep(wait_time)
+        gc.collect()
 
-if __name__ == "__main__":
-    main()
---
+++ modbus_led.py
@@ -0,0 +1,230 @@
+import serial,re,time
+
+DEBUG_VERBOSE=False
+PORT="/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_B001K6BE-if00-port0"
+#PORT="/dev/ttyS1"
+BAUDRATE=115200 #9600
+SF=b"\xAA\xA5"
+GID=b""
+SRC=b"\x00\x00"
+TID=b"\xB0\xA1"
+CRC=b"\x00\x00"
+EF=b"\x5A\x55"
+# first low byte then high byte
+# SF 2 | CTRL 2 | DES 2 | SRC 2 | TID 2 | CMD 2 | ... | CRC 2 | EF 2
+# SF fixed: AA A5
+# EF fixed: 5A 55
+# CTRL 2 bytes = 16bits: 0-10: length; 11: CRC (0 off); 12-15: reserve; thus high byte is 0. and .<8
+def send_modbus(tx_data):
+    #tx_data = bytes.fromhex("AA A5 08 00 FF FF 00 00 B0 A1 01 00 00 00 5A 55")
+    ser = serial.Serial(port=PORT,baudrate=BAUDRATE,parity=serial.PARITY_NONE,stopbits=serial.STOPBITS_ONE,bytesize=serial.EIGHTBITS,timeout=1)
+    ser.reset_input_buffer()
+    a = ser.write(tx_data)
+    time.sleep(0.1)
+    rx_data = ser.read_all()
+    #rx_data = ser.read(size=18)
+    ser.close()
+    return rx_data
+
+def get_response(DES,CMD,CNT=b''):
+    CTRL = int(8+len(CNT)).to_bytes(2,byteorder='little')
+    rx_data = send_modbus(SF+CTRL+DES+SRC+TID+CMD+CNT+CRC+EF)
+    rx_len=len(rx_data)
+    if not rx_data[:2]==SF:
+        if DEBUG_VERBOSE:
+            print("SF flag not match!",rx_data[:2])
+        return
+    if not rx_data[8:10]==TID:
+        if DEBUG_VERBOSE:
+            print("TID not match!",TID,"vs",rx_data[8:10])
+        return
+    if not rx_data[12:14]==b"\x00\x00":
+        if DEBUG_VERBOSE:
+            print("ERROR code:",rx_data[12:14].hex())
+        return
+    if not rx_data[(rx_len-2):]==EF:
+        if DEBUG_VERBOSE:
+            print("EF flag not match!",rx_data[16:])
+    return rx_data
+
+def get_GID():
+    global GID
+    GID = get_response(b"\xFF\xFF",b"\x01\x00")[6:8]
+
+def get_W_H():
+    rx_data = get_response(GID,b"\x1F\x00")
+    if rx_data is None:
+        return
+    W = int.from_bytes(rx_data[14:16],byteorder='little')
+    H = int.from_bytes(rx_data[16:18],byteorder='little')
+    return W,H
+
+def get_brightness():
+    rx_data = get_response(GID,b"\x37\x00")
+    if rx_data is None:
+        return
+    return int.from_bytes(rx_data[14:16],byteorder='little')
+
+def set_brightness(lum):
+    if lum<0 or lum>990:
+        if DEBUG_VERBOSE:
+            print("Out of brightness rage, set to 500")
+        lum=500    
+    rx_data = get_response(GID,b"\x38\x00",int(lum).to_bytes(2,byteorder='little'))
+
+def get_text_color():
+    rx_data = get_response(GID,b"\x43\x00")
+    if rx_data is None:
+        return
+    color=["","F00","0F0","FF0","00F","F0F","0FF","FFF","000"]
+    return color[int(rx_data[14])]
+
+def set_text_color(col):
+    if len(col)>3 or not set(col).issubset({'F','0'}):
+        if DEBUG_VERBOSE:
+            print("Incorrect color code:",col,"\tset it to white")
+        col="FFF"
+    col = re.sub("F","1",col)[::-1]
+    col = "1000".zfill(8) if col=='000' else col.zfill(8)
+    col = int(col, 2).to_bytes(1)
+    rx_data = get_response(GID,b"\x44\x00",col)
+
+def get_paint_color():
+    rx_data = get_response(GID,b"\x4B\x00")
+    if rx_data is None:
+        return
+    color=["","F00","0F0","FF0","00F","F0F","0FF","FFF","000"]
+    return color[int(rx_data[14])]
+
+def set_paint_color(col):
+    if len(col)>3 or not set(col).issubset({'F','0'}):
+        if DEBUG_VERBOSE:
+            print("Incorrect color code:",col,"\tset it to white")
+        col="FFF"
+    col = re.sub("F","1",col)[::-1]
+    col = "1000".zfill(8) if col=='000' else col.zfill(8)
+    col = int(col, 2).to_bytes(1)
+    rx_data = get_response(GID,b"\x4C\x00",col)
+
+def get_font_number():
+    rx_data = get_response(GID,b"\x00\x01")
+    if rx_data is None:
+        return
+    return int(rx_data[14])
+
+def clear_screen():
+    rx_data = get_response(GID,b"\x00\x02")
+
+def show_text(x,y,w,h,col,txt,multiline=False):
+    if len(col)>3 or not set(col).issubset({'F','0'}):
+        if DEBUG_VERBOSE:
+            print("Incorrect color code:",col,"\tset it to white")
+        col="FFF"
+    X = int(x).to_bytes(2,byteorder='little')
+    Y = int(y).to_bytes(2,byteorder='little')
+    W = int(w).to_bytes(2,byteorder='little')
+    H = int(h).to_bytes(2,byteorder='little')
+    col = re.sub("F","1",col)[::-1]
+    col = "1000" if col=='000' else "0"+col
+    font_id = '00000000'
+    h_align = '00'
+    v_align = '00'
+    others = '0000' if multiline else '0010'
+    FORMAT = int("000000000000"+col+font_id+h_align+v_align+others, 2).to_bytes(4, byteorder='little')
+    CNT = int(len(txt)).to_bytes(2,byteorder='little')
+    rx_data = get_response(GID,b"\x38\x02",X+Y+W+H+FORMAT+CNT+txt.encode('ascii'))
+
+def show_image(x,y,img):
+    #image is a string list with each element is a row, each position in an element is column, 1 means lighting up, 0 means dark
+    PG=b''
+    PN=0
+    for i,r in enumerate(img):
+        for j,p in enumerate(r):
+            if p=="1":
+                PN +=1
+                PG += int(x+j).to_bytes(2,byteorder='little')
+                PG += int(y+i).to_bytes(2,byteorder='little')
+    rx_data = get_response(GID,b"\x04\x02",int(PN).to_bytes(2,byteorder='little')+PG)
+
+def move_frame_left(x,y,w,h):
+    X = int(x).to_bytes(2,byteorder='little')
+    Y = int(y).to_bytes(2,byteorder='little')
+    W = int(w).to_bytes(2,byteorder='little')
+    H = int(h).to_bytes(2,byteorder='little')
+    rx_data = get_response(GID,b"\x44\x02",X+Y+W+H)
+
+def move_frame_right(x,y,w,h):
+    X = int(x).to_bytes(2,byteorder='little')
+    Y = int(y).to_bytes(2,byteorder='little')
+    W = int(w).to_bytes(2,byteorder='little')
+    H = int(h).to_bytes(2,byteorder='little')
+    rx_data = get_response(GID,b"\x45\x02",X+Y+W+H)
+
+def move_frame_up(x,y,w,h):
+    X = int(x).to_bytes(2,byteorder='little')
+    Y = int(y).to_bytes(2,byteorder='little')
+    W = int(w).to_bytes(2,byteorder='little')
+    H = int(h).to_bytes(2,byteorder='little')
+    rx_data = get_response(GID,b"\x46\x02",X+Y+W+H)
+
+def move_frame_down(x,y,w,h):
+    X = int(x).to_bytes(2,byteorder='little')
+    Y = int(y).to_bytes(2,byteorder='little')
+    W = int(w).to_bytes(2,byteorder='little')
+    H = int(h).to_bytes(2,byteorder='little')
+    rx_data = get_response(GID,b"\x47\x02",X+Y+W+H)
+
+#create_canvas(2,0,0,64,16)
+#create_canvas(1,0,16,192,16)
+def create_canvas(wid,x,y,w,h):
+    WID = int(wid).to_bytes(2,byteorder='little')
+    OP = b'\x00\x00'
+    X = int(x).to_bytes(2,byteorder='little')
+    Y = int(y).to_bytes(2,byteorder='little')
+    W = int(w).to_bytes(2,byteorder='little')
+    H = int(h).to_bytes(2,byteorder='little')
+    STYLE= b'\x00\x00\x00\x00'
+    USR=b'\x00\x00\x00\x00'
+    rx_data = get_response(GID,b"\x03\x03",WID+OP+X+Y+W+H+STYLE+USR)
+
+#create_txt_programe(1,'0FF',2,5,0,2,200,"Southwest Phoenix-San Diego Boeing 737-7H4")
+#create_txt_programe(1,'F0F',4,5,0,0,0,"HA16 HNL-SAN A330")
+#create_txt_programe(2,'F0F',4,5,200,4,5,"HA16\nHNL-SAN\nA330",True)
+def create_txt_programe(wid,col,en,sp,du,ex,repeat,txt,multiline=False):
+    WID = int(wid).to_bytes(2,byteorder='little')
+    REV = b'\x00\x00'
+    STYLE= b'\x00\x00\x00\x00'
+    if len(col)>3 or not set(col).issubset({'F','0'}):
+        if DEBUG_VERBOSE:
+            print("Incorrect color code:",col,"\tset it to white")
+        col="FFF"
+    col = re.sub("F","1",col)[::-1]
+    col = "1000" if col=='000' else "0"+col
+    font_id = '00000000'
+    h_align = '00'
+    v_align = '00'
+    others = '0000' if multiline else '0010'
+    FORMAT = int("000000000000"+col+font_id+h_align+v_align+others, 2).to_bytes(4, byteorder='little')
+    ENTRY = int(en).to_bytes(2,byteorder='little')
+    SPENTRY = int(sp).to_bytes(2,byteorder='little')
+    DUENTRY = int(du).to_bytes(2,byteorder='little')
+    HIGHLIGHT = b'\x00\x00'
+    SPHL = b'\x00\x00'
+    DUHL = b'\x00\x00' 
+    EXIT = int(ex).to_bytes(2,byteorder='little')
+    SPEXIT = SPENTRY
+    TIMES = int(repeat).to_bytes(2,byteorder='little')
+    CNT = int(len(txt)).to_bytes(2,byteorder='little')
+    rx_data = get_response(GID,b"\x10\x03",WID+REV+STYLE+FORMAT+ENTRY+SPENTRY+DUENTRY+HIGHLIGHT+SPHL+DUHL+EXIT+SPEXIT+TIMES+CNT+txt.encode('ascii'))
+
+@staticmethod
+def calculate_modbus_crc(data):
+    crc = 0xFFFF
+    for byte in data:
+        crc ^= byte
+        for _ in range(8):
+            if crc & 0x0001:
+                crc = (crc >> 1) ^ 0xA001
+            else:
+                crc >>= 1
+    return crc.to_bytes(2, 'little')
